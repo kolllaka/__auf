b7aa190112ffe2bb59d21910f3517834f94d7692
Denis <kotshredengera@gmail.com>
2023-07-28 02:09:53 +0300

    swiper

--- src/js/main.js
+++ src/js/main.js
@@ -70,128 +70,18 @@ function fixedNav() {
 window.addEventListener('scroll', fixedNav)
 
 
-// Select
-const selectes = document.querySelectorAll('select');
-function create(select) {
-	var options = select.querySelectorAll('option');
-
-	mySelect = document.createElement("div");
-	mySelect.setAttribute("class", "select__choose");
-	select.parentElement.appendChild(mySelect);
-
-	mySelect.insertAdjacentHTML('beforeEnd',
-		'<div class="select__selected"></div>' +
-		'<div class="select__icon"><span></span></div>');
-
-	myUl = document.createElement("ul");
-	myUl.setAttribute("class", "select__options");
-	select.parentElement.appendChild(myUl);
-
-	options.forEach((option, index) => {
-		myLi = document.createElement('li');
-		myLi.setAttribute('class', 'select__option');
-		myLi.innerHTML = option.innerHTML;
-		if (option.selected) {
-			console.log(option.selected);
-			myLi.classList.toggle('selected');
-		}
-		myUl.appendChild(myLi);
-
-	});
-};
-
-selectes.forEach(select => {
-	create(select);
-});
-
-const customSelectes = document.querySelectorAll('.select');
-customSelectes.forEach(select => {
-	let selectBtn = select.querySelector('.select__choose');
-	let selectOpts = select.querySelector('.select__options');
-	const selectOptions = select.querySelectorAll('.select__option');
-
-	var value = select.querySelector('select').value;
-	update(select, value);
-
-	selectBtn.addEventListener('click', e => {
-
-		selectBtn.classList.toggle('active');
-		selectOpts.classList.toggle('active');
-	});
-
-	selectOptions.forEach((selectOption, index) => {
-
-		selectOption.addEventListener('click', e => {
-			selectOptions.forEach(selectOption => {
-
-				selectOption.classList.remove('selected')
-			});
-
-			e.target.classList.toggle('selected')
-			update(select, index);
-		});
-	});
-});
-
-function update(select, index) {
-	let selectTitle = select.querySelector('.select__selected');
-	let selectSelected = select.querySelector('.selected');
-
-	select.querySelectorAll('option').forEach((option, i) => {
-		option.selected = false;
-		if (i == index) {
-			option.selected = true;
-		};
-	});
-
-	selectTitle.innerHTML = selectSelected.innerHTML;
-};
-
-
 // Swiper
-// BildSlider
-let sliders = document.querySelectorAll('._swiper');
-if (sliders) {
-	for (let index = 0; index < sliders.length; index++) {
-		let slider = sliders[index];
-		if (!slider.classList.contains('swiper-bild')) {
-			let slider_items = slider.children;
-			if (slider_items) {
-				for (let index = 0; index < slider_items.length; index++) {
-					let el = slider_items[index];
-					el.classList.add('swiper-slide');
-				}
-			}
-			let slider_content = slider.innerHTML;
-			let slider_wrapper = document.createElement('div');
-			slider_wrapper.classList.add('swiper-wrapper');
-			slider_wrapper.innerHTML = slider_content;
-			slider.innerHTML = '';
-			slider.appendChild(slider_wrapper);
-			slider.classList.add('swiper-bild');
-		}
-		if (slider.classList.contains('_gallery')) {
-			//slider.data('lightGallery').destroy(true);
-		}
-	}
-	sliders_bild_callback();
-}
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	slidesPerView: 3,
+	centeredSlides: true,
+	centerInsufficientSlides: true,
+	spaceBetween: 60,
 
-function sliders_bild_callback(params) { };
+	// If we need pagination
+	pagination: {
+		el: document.querySelector('.slider__dotts'),
+		clickable: true,
+	},
+});
 
-if (document.querySelector('.slider')) {
-	let sliders = document.querySelectorAll('.slider');
-	for (let index = 0; index < sliders.length; index++) {
-		let slider = sliders[index];
-		let sliderBody = slider.querySelector('.slider__body');
-		let itemSlider = new Swiper(sliderBody, {
-			observer: true,
-			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
-			autoHeight: true,
-			speed: 800,
-			loop: true,
-		});
-	}
-}
